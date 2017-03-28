a043c429d99c538ba4e56ca7e7071d766ea9cc10
DR <d.retnan@nhubnigeria.com>
2017-03-28 21:05:16 +0000

    updating dns should work

---
+++ README.md
@@ -0,0 +1,1 @@
+curl -X PUT -H "Content-Type: application/json" -d '{"NameServers":["ns1.gorillahead.com", "ns2.gorillahead.net", "ns3.gorillahead.org"]' https://techpool-dretnan.c9users.io/v1/whois/nameserver/resellerdocs.com
--- routes/whois/dns.js
+++ routes/whois/dns.js
@@ -1,4 +1,5 @@
 var Domain = require('../../lib/whois/lib.js');
+var _ = require('underscore');
 
 	/*
 	* Whois Search (Using Enom)
@@ -15,12 +16,14 @@ module.exports = function (app) {
 		Domain.getCurrentNameServers({sld: domain[0], tld: domain[1] || 'com'}).then((result)=>{
     		res.status(200).json(result);
     	}).catch((error)=>{
-    		res.status(500).json(error);	
+    		res.status(500).json(error);
     	});
 	});
 	
 	app.put('/v1/whois/nameserver/:domain', (req, res, next) => {
-		Domain.setCustomNameServer({}).then((result)=>{
+		var domain = req.params.domain.split('.');
+		_.extend(req.body, {sld: domain[0], tld: domain[1]});
+		Domain.setCustomNameServer(req.body).then((result)=>{
     		res.status(200).json(result);
     	}).catch((error)=>{
     		res.status(500).json(error);	
@@ -29,6 +32,7 @@ module.exports = function (app) {
 	
 	app.get('/v1/whois/nameserver/ng/:domain', (req, res, next) => {
 		var domain = req.params.domain.split('.');
+		// Things to ponder about: ".com.ng" split would fail
 		res.status(200).json({message: "Not Implemented"});
 	});
 	
