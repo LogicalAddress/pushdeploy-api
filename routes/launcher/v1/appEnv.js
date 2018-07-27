5d2d640ade8bc177eaae2f8bd58ea53ea5b2a319
DR <d.retnan@nhubnigeria.com>
2018-07-27 01:07:18 +0000

    appEnv fix

--- routes/launcher/v1/appEnv.js
+++ routes/launcher/v1/appEnv.js
@@ -24,7 +24,14 @@ module.exports = function (app) {
 					opts.host = _app.server.ipv4;
 					opts.username = _app.server.username || 'ubuntu';
 					// opts.privateKey = req.techpool.credentials.custom_private_key;
-					opts.privateKey = fs.readFileSync(__dirname + '/../../launcher.pem');
+					// opts.privateKey = fs.readFileSync(__dirname + '/../../launcher.pem');
+					if(_app.server.provider === "custom"){
+						opts.privateKey = req.techpool.credentials.custom_private_key;//TODO: Copy to _server during setup for custom
+					}else if(_app.server.provider === "aws"){
+						opts.privateKey = _app.server.private_key;
+					}else{
+						throw new Error("Invalid Server Provider. How did we get here?");
+					}
 					var remoteFile = '/home/'+opts.username+'/.cargospace/'+_app.app_name+'.sh';
 					console.log("Remote file to get", remoteFile);
 					opts.session = [
