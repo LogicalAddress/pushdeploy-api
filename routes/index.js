368796b2604b829048f895296333c1aac93ec8c5
DR <d.retnan@nhubnigeria.com>
2018-07-30 11:34:04 +0000

    letsencrypt challenge

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@ module.exports = function (app) {
 	app.get('/', function (req, res, next) {
 		res.send("TechPool");
 	});
-	
+	/* ACME CHALLENGE */
+	app.get('/.well-known/acme-challenge/:letsencrypt', function (req, res, next) {
+		var request = req.params.letsencrypt;
+		if(request.substr(0, 3) == 'key'){
+			return res.status(200).set("Content-Type", "text/plain").send(request);
+		}else{
+			return res.status(200).set("Content-Type", "text/plain").send(request);
+		}
+	});
 };
