1d63153d5c5a420a1a477f2551e31ed4f984d949
Retnan <dretnan@logicaladdress.com>
2020-05-03 21:17:29 +0100

    ci setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
     },
     shell: {
       jasmine: {
-        command: 'jasmine --stop-on-failure',
+        command: 'jasmine',
         options: {
           stdout: true,
           stderr: true,
